6602c3bdd163ee637be37886bd5e0e058c5417eb
firefist10 <kalradev@outlook.com>
2022-05-20 11:57:29 +0530

    supply transaction list item component

---
+++ src/modules/dashboard/lists/SupplyTransactionsList/SupplyTransactionListItem.tsx
@@ -0,0 +1,48 @@
+import { Trans } from '@lingui/macro';
+import { Button, Tooltip, Typography } from '@mui/material';
+import { ListColumn } from 'src/components/lists/ListColumn';
+import { ListItem } from 'src/components/lists/ListItem';
+import { TokenIcon } from 'src/components/primitives/TokenIcon';
+import { useProtocolDataContext } from 'src/hooks/useProtocolDataContext';
+import { ListValueColumn } from '../ListValueColumn';
+import { ParsedEvent } from './SupplyTransactionsList';
+
+export const SupplyTransactionListItem = ({ event }: { event: ParsedEvent }) => {
+  const { currentNetworkConfig } = useProtocolDataContext();
+  const { txhash, amount, iconSymbol, symbol, name } = event;
+  return (
+    <ListItem>
+      <ListColumn maxWidth={160} isRow>
+        <TokenIcon symbol={iconSymbol} fontSize="large" />
+        <Tooltip title={`${name} (${symbol})`} arrow placement="top">
+          <Typography variant="subheader1" sx={{ ml: 3 }} noWrap data-cy={`assetName`}>
+            {symbol}
+          </Typography>
+        </Tooltip>
+      </ListColumn>
+
+      <ListColumn>
+        <Trans>{dotify(txhash, 7)}</Trans>
+      </ListColumn>
+
+      <ListColumn>
+        <ListValueColumn value={amount.toNumber()} />
+      </ListColumn>
+
+      <ListColumn>
+        <Button
+          variant="contained"
+          onClick={() => open(currentNetworkConfig.explorerLink + '/tx/' + txhash)}
+        >
+          <Trans>Explorer</Trans>
+        </Button>
+      </ListColumn>
+    </ListItem>
+  );
+};
+
+function dotify(txHash: string, charSize: number): string {
+  return (
+    txHash.slice(0, charSize) + '....' + txHash.slice(txHash.length - charSize - 1, txHash.length)
+  );
+}
